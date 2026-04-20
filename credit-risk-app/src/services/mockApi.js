1e59c38be4820e21449d3981560ea11a291ecd86
Latish K. <160815195+Latish14@users.noreply.github.com>
2026-04-20 23:20:30 +0530

    Update mockApi.js

--- credit-risk-app/src/services/mockApi.js
+++ credit-risk-app/src/services/mockApi.js
@@ -2,21 +2,19 @@ const API_URL = "https://xai-credit-score-system.onrender.com";
 
 export const predictRisk = async (form) => {
   try {
-    // ✅ FIXED mapping
     const formattedData = {
-      loan_amnt: Number(form.loan_amount),
-      int_rate: Number(form.int_rate),
-      annual_inc: Number(form.annual_income),
-      dti: Number(form.dti),
-      fico_range_low: Number(form.fico_score),
+  loan_amnt: Number(form.loanAmount),
+  int_rate: Number(form.interestRate),
+  annual_inc: Number(form.annualIncome),
+  dti: Number(form.dti),
+  fico_range_low: Number(form.ficoScore),
 
-      // safe defaults (optional fields)
-      term: "36 months",
-      grade: "B",
-      home_ownership: "RENT",
-      verification_status: "Verified",
-      purpose: "debt_consolidation"
-    };
+  term: "36 months",
+  grade: "B",
+  home_ownership: "RENT",
+  verification_status: "Verified",
+  purpose: "debt_consolidation"
+};
 
     console.log("Sending to backend:", formattedData);
 
